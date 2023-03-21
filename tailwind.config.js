31865a7ae7999879ee20336808216a169be77d28
HyeonWoo <rmakxh@naver.com>
2023-03-21 21:01:07 +0900

    conf: Add youtube color theme in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        youtube: '#FF0000',
+      },
+    },
   },
   plugins: [require('@tailwindcss/line-clamp')],
 };
