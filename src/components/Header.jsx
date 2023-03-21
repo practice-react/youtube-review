768182a2fdfd1d4bc9e92dc0ee5c8870c362c238
HyeonWoo <rmakxh@naver.com>
2023-03-21 20:59:18 +0900

    feat: Add Html and style in Header compoent

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { BsYoutube } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import SearchVideos from './SearchVideos';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className='flex items-center w-full gap-8 px-8 py-4 bg-black'>
+      <Link to='/'>
+        <div className='flex w-full items-center gap-3 text-3xl basis-2/6'>
+          <BsYoutube className='text-youtube' />
+          <h1 className='text-white'>YOUTUBE</h1>
+        </div>
+      </Link>
+      <SearchVideos />
+    </header>
+  );
 }
