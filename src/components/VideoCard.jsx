00d15d04633e5a592652d9e285c171803a5b67ce
HyeonWoo <rmakxh@naver.com>
2023-03-22 14:54:24 +0900

    feat: Set date -> use timaago lib

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import dateConversion from '../util/data';
+import formatAgo from '../util/date';
+import { useNavigate } from 'react-router-dom';
 
-export default function VideoCard({ video }) {
+export default function VideoCard({ video, id }) {
   const { thumbnails, title, description, publishedAt } = video;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/videos/${id}`, {
+      state: { video, id },
+    });
+  };
 
   return (
-    <div className=''>
-      <img src={thumbnails.high.url} alt={title} className='min-w-full' />
+    <div className='hover:cursor-pointer' onClick={handleClick}>
+      <img src={thumbnails.high.url} alt={title} className='min-w-full ' />
       <div className='p-2'>
         <h2 className='line-clamp-2'>{title}</h2>
         <p className='mt-2 opacity-50 text-sm line-clamp-2'>{description}</p>
-        <time className='text-xs '>{dateConversion(publishedAt)}</time>
+        <time className='text-xs '>{formatAgo(publishedAt, 'ko')}</time>
       </div>
     </div>
   );
