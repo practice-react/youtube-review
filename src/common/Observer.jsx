9543361e39fbec865f11cdd479f04eb20b823b8a
HyeonWoo <rmakxh@naver.com>
2023-03-23 20:46:09 +0900

    feat: Create intersectionObserver api Component
    - intersectionObserver를 사용해 데이터를 재요청할 수 있게 하는 컴포넌트 생성

---
+++ src/common/Observer.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useRef } from 'react';
+// 만약 Observer에서 useQuery 생성했다면 어떻게 됐을까?
+// 일단 props로 받아야 할것은 nextPageToken과 id, type,setComments이다.
+// useQuery(['comments',id,nextPageToken],()=>{ return youtube[type](id,nextPageToken)})
+// 받아온 데이터를 이용해 state를 변경하겠지.
+// 뭐가 더 좋을까?
+
+export default function Observer({ nextComments, token }) {
+  const observerRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && entry.intersectionRatio > 0) {
+          nextComments(token);
+        }
+      });
+    });
+    observer.observe(observerRef.current);
+    return () => observer.disconnect(observerRef.current);
+  }, [token]);
+
+  return (
+    <div ref={observerRef}>
+      <div className='w-20 h-20 rounded-full flex justify-center items-center	bg-gradient-to-r from-cyan-500 to-blue-500 animate-spin'>
+        <div className='bg-white w-10 h-10 rounded-full'></div>
+      </div>
+    </div>
+  );
+}
